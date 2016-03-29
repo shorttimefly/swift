067d73247955f5e5fa145259cd4c5af6bb70b226
Max <max@maxdemacbook-air.local>
2016-03-29 12:40:43 +0800

    go

---
+++ weatherTest/weatherTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherTest
+//
+//  Created by Max on 16/3/29.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
