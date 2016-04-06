39be2fceab144fe4f4ee7422ec0ec65c84f92ac6
steven <shorttimefly@gmail.com>
2016-04-06 12:06:29 +0800

    star

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Max on 16/4/5.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather/Weather/WeatherApi.swift
@@ -0,0 +1,37 @@
+//
+//  WeatherApi.swift
+//  Weather
+//
+//  Created by Max on 16/4/6.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+import UIKit
+
+class WeatherApi: NSObject{
+    func  request(httpUrl: String, httpArg: String) {
+        let req = NSMutableURLRequest(URL: NSURL(string: httpUrl + "?" + httpArg)!)
+        req.timeoutInterval = 6
+        req.HTTPMethod = "GET"
+        req.addValue("a81d19e6348737b6f835215aeedc55a0", forHTTPHeaderField: "apikey")
+        NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue.mainQueue()) {
+            (response, data, error) in
+            if let _ = error{
+                print("请求失败")
+            }
+            let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments)
+            let weatherData  = json["retData"]
+            
+            let cityName = weatherData?!.objectForKey("city") as! String
+            //let htmpData = weatherData?!.objectForKey("h_tmp") as! String
+            //let ltmpData = weatherData?!.objectForKey("l_tmp") as! String
+            let cityWeather = weatherData?!.objectForKey("temp") as! String
+            //let timeData = weatherData?!.objectForKey("time") as! String
+            //let dateData = weatherData?!.objectForKey("date") as! String
+            //dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            //})
+        }
+    }
+    
+
+}
